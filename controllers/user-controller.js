7fd3b50decd877952c02b1e64f8e7aa4d7f61a56
Bill Morris <bilcon@swbell.net>
2022-01-30 12:27:28 -0600

    finished user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,9 +19,9 @@ const userController = {
 
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      .select("-_v")
       .populate("friends")
       .populate("thoughts")
+      .select("-_v")
       .then((dbUserData) => {
         if (!dbUserData) {
           return res.status(404).json({ message: "No user with this ID" });
@@ -33,4 +33,59 @@ const userController = {
         res.status(500).json(err);
       });
   },
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: { friendId: params.friendId } } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
+
+module.exports = userController;
